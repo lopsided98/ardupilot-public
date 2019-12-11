490bad7dfc13e6316f3f1a9ead2804ddf5a6bd94
Ben Wolsieffer <benwolsieffer@gmail.com>
2019-12-10 20:18:55 -0500

    Add GitHub Actions CI and Nix support

---
+++ default.nix
@@ -0,0 +1,49 @@
+{ pkgs ? import <nixpkgs> {}, board ? "omnibusf4pro", dev ? false }:
+with pkgs;
+
+stdenv.mkDerivation {
+  name = "ardupilot";
+
+  # Don't copy source directory to store if we are running in nix-shell
+  src = if lib.inNixShell then null else lib.cleanSourceWith {
+    filter = name: type: let baseName = baseNameOf (toString name); in !(
+      (baseName == "build" && type == "directory")
+    );
+    src = ./.;
+  };
+
+  nativeBuildInputs = [ which gawk gcc-arm-embedded git ] ++
+    (with python3Packages; [ future pyserial empy pexpect setuptools ]) ++
+    lib.optional dev [ glibcLocales astyle mavproxy procps ccls ];
+
+  postPatch = ''
+    patchShebangs waf
+  '';
+
+  # Prevent waf from using the host compiler
+  preConfigure = ''
+    unset CXX CC OBJCOPY SIZE
+  '';
+
+  configurePhase = ''
+    runHook preConfigure
+    ./waf configure --board '${board}'
+    runHook postConfigure
+  '';
+
+  buildPhase = ''
+    runHook preBuild
+    ./waf rover
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mv 'build/${board}/bin' "$out"
+    runHook postInstall
+  '';
+
+  shellHook = ''
+    runHook preConfigure
+  '';
+}
