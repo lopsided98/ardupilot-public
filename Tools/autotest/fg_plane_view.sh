03f5e7c9b3d6d58105fc3a69ee28b5e741391617
Ben Wolsieffer <benwolsieffer@gmail.com>
2021-03-21 21:31:08 -0400

    Tools/autotest: improve SITL integration with JSBSim/Flightgear

--- Tools/autotest/fg_plane_view.sh
+++ Tools/autotest/fg_plane_view.sh
@@ -3,13 +3,15 @@
 AUTOTESTDIR=$(dirname $0)
 
 nice fgfs \
-    --native-fdm=socket,in,10,,5503,udp \
+    --native-fdm=socket,in,60,,5503,udp \
     --fdm=external \
     --aircraft=Rascal110-JSBSim \
     --fg-aircraft="$AUTOTESTDIR/aircraft" \
     --airport=YKRY \
     --geometry=650x550 \
     --bpp=32 \
+    --prop:/sim/rendering/multi-sample-buffers=1 \
+    --prop:/sim/rendering/multi-samples=4 \
     --disable-hud-3d \
     --disable-horizon-effect \
     --timeofday=noon \
