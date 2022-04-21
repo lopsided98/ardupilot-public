lopsided98/ardupilot

Rover: don't move mainsail when disarmed

APMrover2/AP_MotorsUGV.cpp
@@ -261,7 +261,7 @@ void AP_MotorsUGV::output(bool armed, float ground_speed, float dt)
     output_omni(armed, _steering, _throttle, _lateral);
 
     // output to mainsail
-    output_mainsail();
+    output_mainsail(armed);
 
     // send values to the PWM timers for output
     SRV_Channels::calc_pwm();
@@ -787,13 +787,21 @@ void AP_MotorsUGV::output_throttle(SRV_Channel::Aux_servo_function_t function, f
 }
 
 // output for sailboat's mainsail
-void AP_MotorsUGV::output_mainsail()
+void AP_MotorsUGV::output_mainsail(bool armed)
 {
     if (!has_sail()) {
         return;
     }
 
-    SRV_Channels::set_output_scaled(SRV_Channel::k_mainsail_sheet, _mainsail);
+    if (armed) {
+        SRV_Channels::set_output_scaled(SRV_Channel::k_mainsail_sheet, _mainsail);
+    } else {
+        if (_disarm_disable_pwm) {
+            SRV_Channels::set_output_limit(SRV_Channel::k_mainsail_sheet, SRV_Channel::SRV_CHANNEL_LIMIT_ZERO_PWM);
+        } else {
+            SRV_Channels::set_output_limit(SRV_Channel::k_mainsail_sheet, SRV_Channel::SRV_CHANNEL_LIMIT_TRIM);
+        }
+    }
 }
 
 // slew limit throttle for one iteration

APMrover2/AP_MotorsUGV.h
@@ -139,7 +139,7 @@ protected:
     void output_throttle(SRV_Channel::Aux_servo_function_t function, float throttle, float dt = 0.0f);
 
     // output for sailboat's mainsail in the range of 0 to 100
-    void output_mainsail();
+    void output_mainsail(bool armed);
 
     // true if the vehicle has a mainsail
     bool has_sail() const;

lopsided98/ardupilot-public

libraries/AR_Motors/AP_MotorsUGV.cpp
@@ -307,7 +307,7 @@ void AP_MotorsUGV::output(bool armed, float ground_speed, float dt)
     output_omni(armed, _steering, _throttle, _lateral);
 
     // output to sails
-    output_sail();
+    output_sail(armed);
 
     // send values to the PWM timers for output
     SRV_Channels::calc_pwm();
@@ -911,13 +911,21 @@ void AP_MotorsUGV::output_throttle(SRV_Channel::Aux_servo_function_t function, f
 }
 
 // output for sailboat's sails
-void AP_MotorsUGV::output_sail()
+void AP_MotorsUGV::output_sail(bool armed)
 {
     if (!has_sail()) {
         return;
     }
 
-    SRV_Channels::set_output_scaled(SRV_Channel::k_mainsail_sheet, _mainsail);
+    if (armed) {
+        SRV_Channels::set_output_scaled(SRV_Channel::k_mainsail_sheet, _mainsail);
+    } else {
+        if (_disarm_disable_pwm) {
+            SRV_Channels::set_output_limit(SRV_Channel::k_mainsail_sheet, SRV_Channel::Limit::ZERO_PWM);
+        } else {
+            SRV_Channels::set_output_limit(SRV_Channel::k_mainsail_sheet, SRV_Channel::Limit::TRIM);
+        }
+    }
     SRV_Channels::set_output_scaled(SRV_Channel::k_wingsail_elevator, _wingsail);
     SRV_Channels::set_output_scaled(SRV_Channel::k_mast_rotation, _mast_rotation);
 }

libraries/AR_Motors/AP_MotorsUGV.h
@@ -168,7 +168,7 @@ private:
     void output_throttle(SRV_Channel::Aux_servo_function_t function, float throttle, float dt = 0.0f);
 
     // output for sailboat's mainsail in the range of 0 to 100 and wing sail in the range +- 100
-    void output_sail();
+    void output_sail(bool armed);
 
     // true if the vehicle has a mainsail or wing sail
     bool has_sail() const;