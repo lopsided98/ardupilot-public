ArduPilot/ardupilot

commit e26f8c1d11378aed6454ec8c5b960c8a4d519b9d

AP_RangeFinder: populate AP_RangeFinder_config.h

libraries/AP_RangeFinder/AP_RangeFinder_Bebop.h
@@ -14,19 +14,7 @@
  */
 #pragma once
 
-#include "AP_RangeFinder.h"
-#include "AP_RangeFinder_Backend.h"
-
-#include <AP_HAL/AP_HAL.h>
-#include <utility>
-
-#ifndef AP_RANGEFINDER_BEBOP_ENABLED
-#define AP_RANGEFINDER_BEBOP_ENABLED \
-    AP_RANGEFINDER_BACKEND_DEFAULT_ENABLED &&                           \
-    (CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BEBOP ||       \
-     CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_DISCO) &&      \
-    defined(HAVE_LIBIIO)
-#endif
+#include "AP_RangeFinder_config.h"
 
 #if AP_RANGEFINDER_BEBOP_ENABLED
 
@@ -158,4 +146,4 @@ private:
 };
 
 
-#endif
+#endif  // AP_RANGEFINDER_BEBOP_ENABLED

libraries/AP_RangeFinder/AP_RangeFinder_BBB_PRU.h
@@ -1,18 +1,10 @@
 #pragma once
 
-#include "AP_RangeFinder.h"
-#include "AP_RangeFinder_Backend.h"
-
-#include <AP_BoardConfig/AP_BoardConfig.h>
-
-#ifndef AP_RANGEFINDER_BBB_PRU_ENABLED
-#define AP_RANGEFINDER_BBB_PRU_ENABLED (                            \
-        AP_RANGEFINDER_BACKEND_DEFAULT_ENABLED &&                   \
-        CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BBBMINI \
-        )
-#endif
+#include "AP_RangeFinder_config.h"
 
 #if AP_RANGEFINDER_BBB_PRU_ENABLED
+
+#include "AP_RangeFinder_Backend.h"
 
 #define PRU0_CTRL_BASE 0x4a322000
 
@@ -54,4 +46,4 @@ private:
 
 };
 
-#endif
+#endif  // AP_RANGEFINDER_BBB_PRU_ENABLED

libraries/AP_RangeFinder/AP_RangeFinder_BLPing.h
@@ -1,10 +1,6 @@
 #pragma once
 
-#include "AP_RangeFinder.h"
-
-#ifndef AP_RANGEFINDER_BLPING_ENABLED
-#define AP_RANGEFINDER_BLPING_ENABLED AP_RANGEFINDER_BACKEND_DEFAULT_ENABLED
-#endif
+#include "AP_RangeFinder_config.h"
 
 #if AP_RANGEFINDER_BLPING_ENABLED
 

libraries/AP_RangeFinder/AP_RangeFinder_Lua.h
@@ -1,9 +1,10 @@
 #pragma once
 
-#include "AP_RangeFinder.h"
-#include "AP_RangeFinder_Backend.h"
+#include "AP_RangeFinder_config.h"
 
-#if AP_SCRIPTING_ENABLED
+#if AP_RANGEFINDER_LUA_ENABLED
+
+#include "AP_RangeFinder_Backend.h"
 
 // Data timeout
 #define AP_RANGEFINDER_LUA_TIMEOUT_MS 500
@@ -30,4 +31,4 @@ private:
     float _distance_m;   // stored data from lua script:
 };
 
-#endif
+#endif  // AP_RANGEFINDER_LUA_ENABLED

libraries/AP_RangeFinder/AP_RangeFinder_SITL.h
@@ -14,15 +14,11 @@
  */
 #pragma once
 
-#include <AP_BoardConfig/AP_BoardConfig.h>
-
-#include "AP_RangeFinder_Backend.h"
-
-#ifndef AP_RANGEFINDER_SIM_ENABLED
-#define AP_RANGEFINDER_SIM_ENABLED (CONFIG_HAL_BOARD == HAL_BOARD_SITL && AP_RANGEFINDER_BACKEND_DEFAULT_ENABLED)
-#endif
+#include "AP_RangeFinder_config.h"
 
 #if AP_RANGEFINDER_SIM_ENABLED
+
+#include "AP_RangeFinder_Backend.h"
 
 class AP_RangeFinder_SITL : public AP_RangeFinder_Backend {
 public:

libraries/AP_RangeFinder/AP_RangeFinder_analog.h
@@ -1,14 +1,12 @@
 #pragma once
 
+#include "AP_RangeFinder_config.h"
+
+#if AP_RANGEFINDER_ANALOG_ENABLED
+
 #include "AP_RangeFinder.h"
 #include "AP_RangeFinder_Backend.h"
 #include "AP_RangeFinder_Params.h"
-
-#ifndef AP_RANGEFINDER_ANALOG_ENABLED
-#define AP_RANGEFINDER_ANALOG_ENABLED AP_RANGEFINDER_BACKEND_DEFAULT_ENABLED
-#endif
-
-#if AP_RANGEFINDER_ANALOG_ENABLED
 
 class AP_RangeFinder_analog : public AP_RangeFinder_Backend
 {
@@ -35,4 +33,4 @@ private:
     AP_HAL::AnalogSource *source;
 };
 
-#endif
+#endif  // AP_RANGEFINDER_ANALOG_ENABLED

libraries/AP_RangeFinder/AP_RangeFinder_config.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <AP_HAL/AP_HAL_Boards.h>
+#include <AP_Scripting/AP_Scripting_config.h>
 
 #ifndef AP_RANGEFINDER_ENABLED
 #define AP_RANGEFINDER_ENABLED 1
@@ -8,4 +9,35 @@
 
 #ifndef AP_RANGEFINDER_BACKEND_DEFAULT_ENABLED
 #define AP_RANGEFINDER_BACKEND_DEFAULT_ENABLED AP_RANGEFINDER_ENABLED
+#endif
+
+#ifndef AP_RANGEFINDER_ANALOG_ENABLED
+#define AP_RANGEFINDER_ANALOG_ENABLED AP_RANGEFINDER_BACKEND_DEFAULT_ENABLED
+#endif
+
+#ifndef AP_RANGEFINDER_BBB_PRU_ENABLED
+#define AP_RANGEFINDER_BBB_PRU_ENABLED (                            \
+        AP_RANGEFINDER_BACKEND_DEFAULT_ENABLED &&                   \
+        CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BBBMINI \
+        )
+#endif
+
+#ifndef AP_RANGEFINDER_BLPING_ENABLED
+#define AP_RANGEFINDER_BLPING_ENABLED AP_RANGEFINDER_BACKEND_DEFAULT_ENABLED
+#endif
+
+#ifndef AP_RANGEFINDER_BEBOP_ENABLED
+#define AP_RANGEFINDER_BEBOP_ENABLED \
+    AP_RANGEFINDER_BACKEND_DEFAULT_ENABLED &&                           \
+    (CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_BEBOP ||       \
+     CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_DISCO) &&      \
+    defined(HAVE_LIBIIO)
+#endif
+
+#ifndef AP_RANGEFINDER_LUA_ENABLED
+#define AP_RANGEFINDER_LUA_ENABLED AP_SCRIPTING_ENABLED
+#endif
+
+#ifndef AP_RANGEFINDER_SIM_ENABLED
+#define AP_RANGEFINDER_SIM_ENABLED (CONFIG_HAL_BOARD == HAL_BOARD_SITL && AP_RANGEFINDER_BACKEND_DEFAULT_ENABLED)
 #endif

lopsided98/ardupilot-public

commit 69d6937815057e688e20064494e8250d604b264b

AP_RangeFinder: fix missing include for Bebop
When the rangefinder config was refactored, this include was
accidentally dropped, causing the build to fail:

> In file included from ../../libraries/AP_RangeFinder/AP_RangeFinder_Bebop.cpp:16:
> ../../libraries/AP_RangeFinder/AP_RangeFinder_Bebop.h:91:60: error: expected class-name before ‘{’ token
>    91 | class AP_RangeFinder_Bebop : public AP_RangeFinder_Backend {
>       |                                                            ^
> compilation terminated due to -Wfatal-errors.

Fixes: e26f8c1d11 ("AP_RangeFinder: populate AP_RangeFinder_config.h")

libraries/AP_RangeFinder/AP_RangeFinder_Bebop.h
@@ -18,6 +18,8 @@
 
 #if AP_RANGEFINDER_BEBOP_ENABLED
 
+#include "AP_RangeFinder_Backend.h"
+
 #include <AP_HAL_Linux/Thread.h>
 
 /*